0249436a26418d7122013058ca21f9b6cc3f1d09
Elizabeth Leshuk <eleshuk@gmail.com>
2021-01-25 18:37:21 -0800

    fixed code to correctly filter data

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -9,24 +9,26 @@ var tableData = data;
 console.log(data);
 
 // Table 
-var tbody = d3.select("tbody");
+
 d3.select("table").attr("class", "table table-striped");
 
+function createTable(data) {
+    var tbody = d3.select("tbody");
+    tbody.html("");
+    data.forEach(function(UFO) {
+        console.log(UFO);
+        var tr = tbody.append("tr")
+        tr.append("td").text(UFO.datetime)
+        tr.append("td").text(UFO.city)
+        tr.append("td").text(UFO.state)
+        tr.append("td").text(UFO.country)
+        tr.append("td").text(UFO.shape)
+        tr.append("td").text(UFO.durationMinutes)
+        tr.append("td").text(UFO.comments)
+    })
+};
+createTable(tableData);
 
-data.forEach(function(UFO) {
-    console.log(UFO);
-    var tr = tbody.append("tr")
-    tr.append("td").text(UFO.datetime)
-    tr.append("td").text(UFO.city)
-    tr.append("td").text(UFO.state)
-    tr.append("td").text(UFO.country)
-    tr.append("td").text(UFO.shape)
-    tr.append("td").text(UFO.durationMinutes)
-    tr.append("td").text(UFO.comments)
-  });
-
-// Use a date form in your HTML document and write JavaScript code that will listen for events and search through the date/time column to find rows that match user input.
-// Select the button
 var button = d3.select("#filter-btn");
 // Select the form
 var inputField = d3.select("#form");
@@ -34,6 +36,11 @@ var inputField = d3.select("#form");
 // Create event handlers 
 button.on("click", runEnter);
 inputField.on("submit",runEnter);
+
+
+// Use a date form in your HTML document and write JavaScript code that will listen for events and search through the date/time column to find rows that match user input.
+// Select the button
+
 
 // Complete the event handler function for the form
 function runEnter() {
@@ -49,36 +56,13 @@ function runEnter() {
   
     console.log(inputValue);
     console.log(tableData);
-  
-    var filteredData = tableData.filter(x => x.datetime === inputValue);
-  
+
+    if (inputValue) {
+        var filteredData = tableData.filter(x => x.datetime === inputValue);
+        }
+    createTable(filteredData);
+
     console.log(filteredData);
+};
 
-    // Display the filtered dataset
 
-  
-    // BONUS: Calculate summary statistics for the age field of the filtered data
-  
-    // First, create an array with just the age values
-    // var ages = filteredData.map(person => person.age);
-  
-    // // Next, use math.js to calculate the mean, median, mode, var, and std of the ages
-    // var mean = math.mean(ages);
-    // var median = math.median(ages);
-    // var mode = math.mode(ages);
-    // var variance = math.var(ages);
-    // var standardDeviation = math.std(ages);
-  
-    // // Then, select the unordered list element by class name
-    // var list = d3.select(".summary");
-  
-    // // remove any children from the list to
-    // list.html("");
-  
-    // append stats to the list
-    // list.append("li").text(`Mean: ${mean}`);
-    // list.append("li").text(`Median: ${median}`);
-    // list.append("li").text(`Mode: ${mode}`);
-    // list.append("li").text(`Variance: ${variance}`);
-    // list.append("li").text(`Standard Deviation: ${standardDeviation}`);
-  };
